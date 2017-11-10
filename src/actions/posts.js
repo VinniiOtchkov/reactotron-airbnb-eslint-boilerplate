8162f2c439a8f20efc841cd1373c709ff14bd925
vinniiotchkov <vinniiotchkov@gmail.com>
2017-11-10 15:20:44 -0700

    Pushing for good measure

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-export const fetchPosts = () => {
-  return {
-    type: 'FETCH',
-    payload: axios.get('https://59e5b56cf99ad900122682ad.mockapi.io/posts')
-  };
-};
+export const fetchPosts = () => ({
+  type: 'FETCH',
+  payload: axios.get('https://59e5b56cf99ad900122682ad.mockapi.io/posts')
+});
 
-export const addPost = (post) => {
-  return {
-    type: 'ADD',
-    payload:axios.post('https://59e5b56cf99ad900122682ad.mockapi.io/posts', post)
-  };
-};
+export const addPost = post => ({
+  type: 'ADD',
+  payload: axios.post('https://59e5b56cf99ad900122682ad.mockapi.io/posts', post)
+});
